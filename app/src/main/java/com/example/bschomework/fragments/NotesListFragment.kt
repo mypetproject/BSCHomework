a181d29f93a55c94bb1ee55ae77e2b9f1e015492
Stanislav Kishkar <skishkarwork@gmail.com>
2021-12-10 09:46:05 +0800

    Фрагмент добавляется в back stack при замене в контейнере.

--- app/src/main/java/com/example/bschomework/fragments/NotesListFragment.kt
+++ app/src/main/java/com/example/bschomework/fragments/NotesListFragment.kt
@@ -8,7 +8,6 @@ import androidx.fragment.app.Fragment
 import com.example.bschomework.NoteData
 import com.example.bschomework.NotesListAdapter
 import com.example.bschomework.R
-import com.example.bschomework.activities.MainActivity
 import com.example.bschomework.databinding.FragmentNotesListBinding
 import com.example.bschomework.presenters.NoteDataFragmentPresenter
 import com.example.bschomework.presenters.NotesListFragmentPresenter
@@ -40,10 +39,11 @@ class NotesListFragment : Fragment(R.layout.fragment_notes_list), NotesListFragm
             it.putParcelable(NoteDataFragmentPresenter.REQUEST_KEY, noteData)
         }
 
-        (activity as MainActivity).supportFragmentManager.beginTransaction().run {
-            replace(R.id.fragment_notes_list, NoteDataFragment().also {
+        parentFragmentManager.beginTransaction().run {
+            replace(R.id.fragment_container, NoteDataFragment().also {
                 it.arguments = bundle
             })
+            addToBackStack(null)
             commit()
         }
     }
